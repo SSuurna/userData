5ce8a1e75a96eda70aa6698939bc7f1139c536fa
Sander Suurna <sande@DESKTOP-8IN12K6>
2021-09-13 02:21:39 +0300

    Control Flow

---
+++ DrivingLisence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DrivingLisence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm arvutab kasutaja vanust
+            //kui kasutaja on noorem kui 18, siis kuvab konsoolis
+            //"oled liiga noor"
+            //kui kasutaja on vanem kui 18, siis kuvab konsoolis
+            //"oled piisavalt vana"
+            //kui kasutaja on 18, siis kuvab konsoolis
+            //"Palju õnne!"
+
+            Console.WriteLine("Palun sisesta oma sünniaasta:");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - YearOfBirth;
+
+            if(userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana!");
+            }
+            else if(userAge < 18)
+            {
+                Console.WriteLine("Oled veel liiga noor!");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne! Oled 18!");
+            }
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanaust
+            //programm arvutab kasutaja sünniaastat
+            //programm kuvab sünniaastat konsoolis.
+            //"oled sündinud aastal {YearOFBirth}"
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int YearOfBirth = 2021 - userAge;
+
+            Console.WriteLine($"Oled sündinud aastal {YearOfBirth}.");
+
+
+         
+            
+        }
+    }
+}
